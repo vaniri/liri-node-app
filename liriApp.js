e74ccbf87930dd266f929d8b7e8a1fceb47c2acf
Vaniri <foxifennec@gmail.com>
2020-01-13 00:29:01 -0500

    added function for Band search

---
+++ liriApp.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const keys = require("./keys.js");
+// const spotify = new Spotify(keys.spotify);
+const axios = require('axios').default;
+const inquirer = require("inquirer");
+
+function initiateSearch() {
+    inquirer
+        .prompt([
+            {
+                name: "band",
+                type: "input",
+                message: "What band you are looking for?"
+            }
+        ])
+        .then(answer => {
+            if (answer.band === "") {
+                console.log("Please enter a band!");
+                return initiateSearch();
+            }
+            let band = answer.band.replace(" ", "+");
+            axios.get(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`)
+                .then(response => {
+                    for (var i = 0; i < response.data.length; i++) {
+                        console.log("Venue Name: " + response.data[i].venue.name);
+                        console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+                        console.log("Date of the Event: " + response.data[i].datetime);
+                        console.log("===============================================");
+                    }
+                })
+        });
+}
+
+initiateSearch();
