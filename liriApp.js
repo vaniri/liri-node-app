930a48b716188c47ff727a049a6a3267212344b2
Vaniri <foxifennec@gmail.com>
2020-01-14 00:22:55 -0500

    added doThis function and refectored the code

--- liriApp.js
+++ liriApp.js
@@ -31,7 +31,7 @@ inquirer
                 break;
         }
     })
-    
+
 
 function initiateSearchForBand() {
     inquirer
@@ -43,22 +43,26 @@ function initiateSearchForBand() {
             }
         ])
         .then(answer => {
-            if (answer.band === "") {
-                console.log("Please enter a band!");
-                return initiateSearchForBand();
-            }
-            let band = answer.band.replace(" ", "+");
-            axios.get(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`)
-                .then(response => {
-                    for (let i = 0; i < response.data.length; i++) {
+            searchForBand(answer.band);
+        })
+}
 
-                        console.log("Venue Name: " + response.data[i].venue.name);
-                        console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
-                        console.log("Date of the Event: " + response.data[i].datetime.replace("T", "\nStart at: "));
-                        console.log("************************************************");
-                    }
-                })
-        });
+function searchForBand(answer) {
+    if (answer === "") {
+        console.log("Please enter a band!");
+        return initiateSearchForBand();
+    }
+    let band = answer.replace(" ", "+");
+    axios.get(`https://rest.bandsintown.com/artists/${band}/events?app_id=codingbootcamp`)
+        .then(response => {
+            for (let i = 0; i < response.data.length; i++) {
+
+                console.log("Venue Name: " + response.data[i].venue.name);
+                console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+                console.log("Date of the Event: " + response.data[i].datetime.replace("T", "\nStart at: "));
+                console.log("\x1b[32m%s\x1b[0m", ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+            }
+        })
 }
 
 function initiateSearchForMovie() {
@@ -71,24 +75,27 @@ function initiateSearchForMovie() {
             }
         ])
         .then(answer => {
-            if (answer.movie === "") {
-                answer.movie = "Mr. Nobody";
-                console.log("If you haven't watched Mr. Nobody, then you should! \nIt's on Netflix! \n************************************************");
-            }
-            axios.get(`http://www.omdbapi.com/?t=${answer.movie}/&y=&plot=short&apikey=trilogy`)
-                .then(response => {
-                    console.log("Title of the movie: " + response.data.Title);
-                    console.log("Year the movie came out: " + response.data.Released);
-                    console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-                    console.log("Rotten Tomatoes Rating of the movie: " + response.data.Rated);
-                    console.log("Country where the movie was produced: " + response.data.Country);
-                    console.log("Language of the movie: " + response.data.Language);
-                    console.log("Plot of the movie: " + response.data.Plot);
-                    console.log("Actors in the movie: " + response.data.Actors);
-                    console.log("************************************************");
+            searchForMovie(answer.movie);
+        })
+}
 
-                })
-        });
+function searchForMovie(movie) {
+    if (movie === "") {
+        movie = "Mr. Nobody";
+        console.log("If you haven't watched Mr. Nobody, then you should! \nIt's on Netflix! \n>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+    }
+    axios.get(`http://www.omdbapi.com/?t=${movie}/&y=&plot=short&apikey=trilogy`)
+        .then(response => {
+            console.log("Title of the movie: " + response.data.Title);
+            console.log("Year the movie came out: " + response.data.Released);
+            console.log("IMDB Rating of the movie: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes Rating of the movie: " + response.data.Rated);
+            console.log("Country where the movie was produced: " + response.data.Country);
+            console.log("Language of the movie: " + response.data.Language);
+            console.log("Plot of the movie: " + response.data.Plot);
+            console.log("Actors in the movie: " + response.data.Actors);
+            console.log("\x1b[34m%s\x1b[0m", ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+        })
 }
 
 function initiateSearchSpotify() {
@@ -101,22 +108,48 @@ function initiateSearchSpotify() {
             }
         ])
         .then(answer => {
-            if (answer.track === "") {
-                answer.track = "The Sign";
-            }
-            spotify
-                .search({ type: 'track', query: answer.track })
-                .then(response => {
-                    for (let track of response.tracks.items){
-                    console.log("Artist: " + track.artists[0].name);
-                    console.log("Song Name: " + track.name);
-                    console.log("Preview Link: " + track.href);
-                    console.log("Album: " + track.album.name);
-                    console.log("************************************************");
-                }
-                })
-                .catch(err => {
-                    throw err;
-                })
+            searchSpotify(answer.track);
         })
+}
+
+function searchSpotify(answer) {
+    if (answer === "") {
+        answer = "The Sign";
+    }
+    spotify
+        .search({ type: 'track', query: answer })
+        .then(response => {
+            for (let track of response.tracks.items) {
+                console.log("Artist: " + track.artists[0].name);
+                console.log("Song Name: " + track.name);
+                console.log("Preview Link: " + track.href);
+                console.log("Album: " + track.album.name);
+                console.log("\x1b[35m%s\x1b[0m", ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+            }
+        })
+        .catch(err => {
+            throw err;
+        })
+}
+
+function doThis() {
+    fs.readFile("random.txt", (err, data) => {
+        if (err) { throw err; }
+        let [command, param] = data.toString().split(",");
+        switch (command) {
+            case "concert-this":
+                searchForBand(param);
+                break;
+            case "spotify-this-song":
+                searchSpotify(param);
+                break;
+            case "movie-this":
+                searchForMovie(param);
+                break;
+            case "do-what-it-says":
+                console.log("Ahaha! Recursion^^");
+                break;
+        }
+        console.log(data.toString());
+    })
 }
