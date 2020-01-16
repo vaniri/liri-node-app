305294c4bbf0cedef1b4a5e4d0e0d572053d64a0
Vaniri <foxifennec@gmail.com>
2020-01-15 23:23:39 -0500

    added function for log and print data

--- liriApp.js
+++ liriApp.js
@@ -1,10 +1,19 @@
 require("dotenv").config();
 const fs = require("fs");
 const keys = require("./keys.js");
-const Spotify = require('node-spotify-api')
+const Spotify = require('node-spotify-api');
 const spotify = new Spotify(keys.spotify);;
 const axios = require('axios').default;
 const inquirer = require("inquirer");
+
+
+let colors = ["30", "31", "32", "33", "34", "35",];
+
+function getColor(arr) {
+    let color = `\x1b[${arr[Math.floor(Math.random() * arr.length)]}m%s\x1b[0m`;
+    return (color);
+}
+let divider = ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<";
 
 inquirer
     .prompt([
@@ -57,10 +66,10 @@ function searchForBand(answer) {
         .then(response => {
             for (let i = 0; i < response.data.length; i++) {
 
-                console.log("Venue Name: " + response.data[i].venue.name);
-                console.log("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
-                console.log("Date of the Event: " + response.data[i].datetime.replace("T", "\nStart at: "));
-                console.log("\x1b[32m%s\x1b[0m", ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+                printLogged("Venue Name: " + response.data[i].venue.name);
+                printLogged("Venue Location: " + response.data[i].venue.city + ", " + response.data[i].venue.country);
+                printLogged("Date of the Event: " + response.data[i].datetime.replace("T", "\nStart at: "));
+                console.log(getColor(colors), divider);
             }
         })
 }
@@ -82,19 +91,20 @@ function initiateSearchForMovie() {
 function searchForMovie(movie) {
     if (movie === "") {
         movie = "Mr. Nobody";
-        console.log("If you haven't watched Mr. Nobody, then you should! \nIt's on Netflix! \n>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+        console.log("If you haven't watched Mr. Nobody, then you should! \nIt's on Netflix!");
+        console.log(getColor(colors), divider);
     }
     axios.get(`http://www.omdbapi.com/?t=${movie}/&y=&plot=short&apikey=trilogy`)
         .then(response => {
-            console.log("Title of the movie: " + response.data.Title);
-            console.log("Year the movie came out: " + response.data.Released);
-            console.log("IMDB Rating of the movie: " + response.data.imdbRating);
-            console.log("Rotten Tomatoes Rating of the movie: " + response.data.Rated);
-            console.log("Country where the movie was produced: " + response.data.Country);
-            console.log("Language of the movie: " + response.data.Language);
-            console.log("Plot of the movie: " + response.data.Plot);
-            console.log("Actors in the movie: " + response.data.Actors);
-            console.log("\x1b[34m%s\x1b[0m", ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+            printLogged("Title of the movie: " + response.data.Title);
+            printLogged("Year the movie came out: " + response.data.Released);
+            printLogged("IMDB Rating of the movie: " + response.data.imdbRating);
+            printLogged("Rotten Tomatoes Rating of the movie: " + response.data.Rated);
+            printLogged("Country where the movie was produced: " + response.data.Country);
+            printLogged("Language of the movie: " + response.data.Language);
+            printLogged("Plot of the movie: " + response.data.Plot);
+            printLogged("Actors in the movie: " + response.data.Actors);
+            console.log(getColor(colors), divider);
         })
 }
 
@@ -120,11 +130,11 @@ function searchSpotify(answer) {
         .search({ type: 'track', query: answer })
         .then(response => {
             for (let track of response.tracks.items) {
-                console.log("Artist: " + track.artists[0].name);
-                console.log("Song Name: " + track.name);
-                console.log("Preview Link: " + track.href);
-                console.log("Album: " + track.album.name);
-                console.log("\x1b[35m%s\x1b[0m", ">>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<");
+                printLogged("Artist: " + track.artists[0].name);
+                printLogged("Song Name: " + track.name);
+                printLogged("Preview Link: " + track.href);
+                printLogged("Album: " + track.album.name);
+                console.log(getColor(colors), divider);
             }
         })
         .catch(err => {
@@ -150,6 +160,15 @@ function doThis() {
                 console.log("Ahaha! Recursion^^");
                 break;
         }
-        console.log(data.toString());
     })
+}
+function log (data) {
+    fs.appendFile("log.txt", data + "\n", (err) => {
+        if (err) { throw err; }
+    });
+}
+
+function printLogged (data) {
+    console.log(data);
+    log(data);
 }
